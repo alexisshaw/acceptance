91c15f164e1d2eb97cc32dc2c81c6501b2a728de
Ben Wright <bwright.au@gmail.com>
2012-04-05 13:07:38 +1000

    Added some nice comments to the top of the files. We cannot expect people to use our tests without comments

--- AcceptanceInterface.java
+++ AcceptanceInterface.java
@@ -1,7 +1,14 @@
+/**
+ * TODO add description.
+ * @author Benjamin James Wright (@ben.wright)
+ * @author Damon Stacey (@damon.stacey)
+ * @version 0.0.1
+ * @since 05.04.2012
+ */
+
 package acceptanceTests;
 
 public interface AcceptanceInterface {
-
    public void createGame(GameState myState);
    public void takeTurn(Move myMove);
    public GameState getState();
--- Card.java
+++ Card.java
@@ -1,3 +1,11 @@
+/**
+ * TODO add description.
+ * @author Benjamin James Wright (@ben.wright)
+ * @author Damon Stacey (@damon.stacey)
+ * @version 0.0.1
+ * @since 05.04.2012
+ */
+
 package acceptanceTests;
 
 public enum Card {
