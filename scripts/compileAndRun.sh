9497a9adb99bc165fb0f6abb1401a61b127c1307
Lasath Fernando <kde@lasath.org>
2012-04-06 13:03:56 +1000

    Fix copyright info in scripts
    I used git blame and some common sense to figure out who is most
    likely to have contributed to each file.

--- scripts/compileAndRun.sh
+++ scripts/compileAndRun.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # requires java, javac.
-# Copyright (C) 2012    Lasath Fernando (@lasath.fernando)
+# Copyright (C) 2012    Lasath Fernando         (@lasath.fernando)
+# Copyright (C) 2012    Benjamin James Wright   (@ben.wright)
+# Copyright (C) 2012    Damon Stacey            (@damon.stacey)
 
 . compile
 
