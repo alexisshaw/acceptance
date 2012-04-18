a4cd439a3ee8356465d781c49183ddf479fcccb1
Lasath Fernando <kde@lasath.org>
2012-04-18 23:02:54 +1000

    Undo a bunch of crap we did in the last commit
    We sort of accidentall broke TestRunner, by committing code that
    I don't even know how found it's way in there.
    
    This commit undoes some of the changes, and fixes up style in the
    file.

--- src/framework/TestRunner.java
+++ src/framework/TestRunner.java
@@ -1,13 +1,11 @@
 package framework;
 
 import framework.interfaces.AcceptanceInterface;
-import framework.interfaces.GameState;
-import framework.interfaces.MoveMaker;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import tests.stable.*;
+import tests.testing.*;
+import tests.unstable.*;
+
 /**
  * Class to run several tests, and handle the results
  *
@@ -19,95 +17,91 @@ import java.util.logging.Logger;
 public class TestRunner {
 
     AcceptanceInterface testee;
-
     int numTestsPassed = 0;
     int numTestFailed = 0;
     int numNotImplemented = 0;
     int numInvalidTests = 0;
 
-   private static Test[] getUnverifiedTests() {
+    private static Test[] getUnverifiedTests() {
 
-       return new Test[]{};
-   }
+        return new Test[]{};
+    }
 
-   private static Test[] getBorderlineTests() {
+    private static Test[] getBorderlineTests() {
 
-       return new Test[]{};
-   }
+        return new Test[]{};
+    }
 
-   private static Test[] getTrueTests() {
+    private static Test[] getTrueTests() {
 
-       return new Test[]{};
-   }
+        return new Test[]{};
+    }
 
-   public static void main (String[] args) {
+    public static void main(String[] args) {
 
-       TestRunner runner = new TestRunner();
-       runner.doYourThing();
-   }
+        TestRunner runner = new TestRunner();
+        runner.doYourThing();
+    }
 
     private void doYourThing() {
-        try {
+        System.out.println("Roma acceptance tests starting...");
 
-              System.out.println ("Roma acceptance tests starting...");
-              boolean assertionsEnabled = false;
-              try {
-                 assert (false);
-              } catch (AssertionError e) {
-                 assertionsEnabled = true;
-              }
-              boolean failed = false;
-              if (!assertionsEnabled) {
-                 System.out.println ("Please enable assertions, run with java -ea");
-                 failed = true;
-              } else {
-                 // step thru all the tests one at a time
-                  runTests (getTrueTests());
-                  runTests (getBorderlineTests());
-                  runTests (getUnverifiedTests());
-              }
-              if (failed) {
-                 System.out.println ("FAILED");
-                 System.out.println ("Not accepted!");
-              } else {
-                 System.out.println ("ACCEPTED");
-                 System.out.println ("You are awesome!");
-              }
-        } catch (InstantiationException ex) {
-            Logger.getLogger(TestRunner.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            Logger.getLogger(TestRunner.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NoSuchMethodException ex) {
-            Logger.getLogger(TestRunner.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SecurityException ex) {
-            Logger.getLogger(TestRunner.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(TestRunner.class.getName()).log(Level.SEVERE, null, ex);
+        boolean assertionsEnabled = false;
+        try {
+            assert (false);
+        } catch (AssertionError e) {
+            assertionsEnabled = true;
+        }
+
+        boolean failed;
+        if (!assertionsEnabled) {
+            System.out.println("Please enable assertions, run with java -ea");
+            failed = true;
+        } else {
+            // step through all the tests one at a time
+            runTests(getTrueTests());
+            runTests(getBorderlineTests());
+            runTests(getUnverifiedTests());
+
+            failed = (numTestFailed > 0) || (numNotImplemented > 0);
+        }
+
+        if (failed) {
+            System.out.println("FAILED");
+            System.out.println("Not accepted!");
+        } else {
+            System.out.println("ACCEPTED");
+            System.out.println("You are awesome!");
         }
     }
 
-       private void runTests (Test[] tests) {
+    private void runTests(Test[] tests) {
 
-       for (Test current : tests) {
-           try {
-               System.out.println("Running Test " + current.getClass() + ":");
-               System.out.println("\t" + current.getShortDescription());
-               current.run(null, null);
-               numTestsPassed++;
-               System.out.println("Test passed");
-           } catch (AssertionError e) {
-               numTestFailed++;
-               System.out.println (current.getOutputSteam());
-               System.out.println("Test Failed");
-           } catch (UnsupportedOperationException e) {
-               numNotImplemented++;
-               System.out.println (current.getOutputSteam());
-               System.out.println("Test Failed");
-           } catch (IllegalArgumentException e) {
-               numInvalidTests++;
-               System.out.println (current.getOutputSteam());
-               System.out.println("Test Failed");
-           }
-       }
-   }
+        for (Test current : tests) {
+            try {
+                System.out.println("Running Test " + current.getClass() + ":");
+                System.out.println("\t" + current.getShortDescription());
+
+                current.run(null, null);
+
+                numTestsPassed++;
+                System.out.println("Test passed");
+
+            } catch (AssertionError e) {
+                numTestFailed++;
+                System.out.println(current.getOutputSteam());
+                System.out.println("Test Failed");
+
+            } catch (UnsupportedOperationException e) {
+                numNotImplemented++;
+                System.out.println(current.getOutputSteam());
+                System.out.println("Test Failed");
+
+            } catch (IllegalArgumentException e) {
+                numInvalidTests++;
+                System.out.println(current.getOutputSteam());
+                System.out.println("Test Failed");
+            }
+        }
+    }
 }
